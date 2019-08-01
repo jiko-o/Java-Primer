05a294eb08fe587ae48eae063c4c607d557475e3
jiko-o <53529673+jiko-o@users.noreply.github.com>
2019-08-01 15:57:52 +0900

    bug fix

--- chap6/Main.java
+++ chap6/Main.java
@@ -1,9 +1,8 @@
-import comment;
 public class Main{
 	public static void main(String[] args) throws Exception{
 		comment.Zenhan.doWarusa();
 		comment.Zenhan.doTogame();
-		//comment.Kouhan.callDeae();
-		//comment.Kouhan.showMondokoro();
+		comment.Kouhan.callDeae();
+		comment.Kouhan.showMondokoro();
 	}
 }
