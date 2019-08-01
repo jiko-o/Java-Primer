dcf0182db005175b4b540e8e63cbe180306b69ab
jiko-o <53529673+jiko-o@users.noreply.github.com>
2019-08-01 15:50:57 +0900

    bug fix

--- chap6/comment/Kouhan.java
+++ chap6/comment/Kouhan.java
@@ -1,10 +1,10 @@
 package comment;
 public class Kouhan{
 	public static void callDeae(){
-		Shstem.out.println("ええい、小癪な。くせ者だ！であえい！");
+		System.out.println("ええい、小癪な。くせ者だ！であえい！");
 	}
 	public static void showMondokoro() throws Exception{
 		System.out.println("飛車さん、もういいでしょい");
-		//Zenhan.doTogame();
+		Zenhan.doTogame();
 	}
 }
