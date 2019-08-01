7772375a0056b826207850d13c4446d6fe25da7c
jiko-o <53529673+jiko-o@users.noreply.github.com>
2019-08-01 13:30:27 +0900

    add files

---
+++ chap4/Exp2.java
@@ -0,0 +1,11 @@
+public class Exp2{
+	public static void main(String[] args){
+		int[] moneyList = {121902, 8302, 55100};
+		for(int i : moneyList){
+			System.out.println(i);
+		}
+		for(int i =0; i < moneyList.length; i++){
+			System.out.println(moneyList[i]);
+		}
+	}
+}
