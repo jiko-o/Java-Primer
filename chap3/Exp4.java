d99cf7cbf0213496928a5dfa6e94aaf7e810e619
jiko-o <53529673+jiko-o@users.noreply.github.com>
2019-08-01 11:57:41 +0900

    add file

---
+++ chap3/Exp4.java
@@ -0,0 +1,13 @@
+public class Exp4{
+	public static void main(String[] args){
+		boolean tenki=true;
+		if (tenki==true){
+			System.out.println("洗濯します");
+			System.out.println("買い物に行きます");
+		}
+		else{
+			System.out.println("DVDを見ます");
+			System.out.println("寝ます");
+		}
+	}
+}
