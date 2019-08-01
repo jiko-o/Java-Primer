a262e4c92e93952fab97aa2ea759540cf6bc7fc4
jiko-o <53529673+jiko-o@users.noreply.github.com>
2019-08-01 14:33:37 +0900

    add files

---
+++ chap6/Main.java
@@ -0,0 +1,9 @@
+import comment;
+public class Main{
+	public static void main(String[] args) throws Exception{
+		comment.Zenhan.doWarusa();
+		comment.Zenhan.doTogame();
+		//comment.Kouhan.callDeae();
+		//comment.Kouhan.showMondokoro();
+	}
+}
---
+++ chap6/comment/Kouhan.java
@@ -0,0 +1,10 @@
+
+public class Kouhan{
+	public static void callDeae(){
+		Shstem.out.println("ええい、小癪な。くせ者だ！であえい！");
+	}
+	public static void showMondokoro() throws Exception{
+		System.out.println("飛車さん、もういいでしょい");
+		//Zenhan.doTogame();
+	}
+}
